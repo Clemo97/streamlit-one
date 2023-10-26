4b8e54d566749ad9e53d089425746b0cb0ffbcdf
Clemo97 <lumumbaclement@gmail.com>
2023-10-26 08:24:28 +0300

    delete show.py

--- show.py
+++
@@ -1,9 +0,0 @@
-# Read the content of an external Python file (e.g., myfile.py)
-with open('myfile.py', 'r') as file:
-    python_code = file.read()
-
-# Create a Streamlit app
-st.title("Display Python Code")
-
-# Display the content of the file in a st.code block
-st.code(python_code, language="python")
